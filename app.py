72e70fafc4196e5dac04bde5e51f9be92bdcbc4c
prathyuv2002 <prathyuv2002@gmail.com>
2026-07-16 18:14:18 -0500

    Clean up app.py by deleting unnecessary code
    Removed unused variables and function definition.

--- app.py
+++ app.py
@@ -1,20 +1,5 @@
-VARIABLES
-4 GAME OVER = False
-5 PLAYER HEALTH = 5
-6 PLAYER FOOD POUNDS = 500
-7 MILES TO GO = 2000
-8 CURRENT DAY = 1
-9 CURRENT MONTH = 3
-10 MONTHS 31 DAYS = [3, 5, 7, 8, 10, 12]
-11 HEALTH DECREASES THIS MONTH = 0
-12
-13
-14 def should decrease health():
-15
-global HEALTH_DECREASES_THIS MONTH
-16
-if HEALTH DECREASES THIS MONTH < 2:
-17
+i deleted  the lines
+i edited it
 random day = random.randint(0, 30)
 18
 if random day < 2:
